20cf4a486d64ab7472d08a405bfdcf09fa0f14de
Christine McWilliams <camcwilliams@wisc.edu>
2021-01-02 17:07:29 -0600

    added flag for 11-15 to make my life easier later

--- make_table.R
+++ make_table.R
@@ -32,11 +32,13 @@ fifteenToSeventeen <- select(nsfg2015_2017, caseid, rscrage, constat1, agebaby1,
 # creating flag to identify the source table
 
 fifteenToSeventeen$fifteenToSeventeen <- 1
+elevenToFifteen$elevenToFifteen <- 1
 
 tb <- bind_rows(elevenToFifteen, fifteenToSeventeen)
 
 table(tb$fifteenToSeventeen, useNA = "always")
 table(fifteenToSeventeen$rscrage > 44)
+table(tb$elevenToFifteen, useNA = "always")
 
 # write table to folder
 
