47baf034a29f92c6c5b37e0a727841fe87663897
Christine McWilliams <camcwilliams@wisc.edu>
2021-01-02 18:10:49 -0600

    dealing with age at first birth

--- var_tx.R
+++ var_tx.R
@@ -77,23 +77,37 @@ str(contraceptors)
 contraceptors$poverty_500 <- ifelse(contraceptors$poverty == 500, 1, 0)
 
 
-# Agebaby1 appears to have different scales for the two different years?
-summary(nsfg2011_2015$agebaby1)
-summary(nsfg2015_2017$agebaby1) # sure enough
+# Dealing with agebaby1 
+
+  # appears to have different scales for the two different years?
+  summary(nsfg2011_2015$agebaby1)
+  summary(nsfg2015_2017$agebaby1) # sure enough
 
 
-  # fixing that
-
-  test_round <- 
+  # fixing that, changing the 11-15 survey's age at first birth to reflect real whole years
+  contraceptors <- 
     contraceptors %>% 
       mutate(agebaby1_round = case_when(
         fifteenToSeventeen == 1 ~ agebaby1,
-        is.na(fifteenToSeventeen) ~ round(agebaby1/100)
+        elevenToFifteen == 1 ~ floor(agebaby1/100)
       ))
   
-    filter(test_round, caseid == 69650)
-
-  histogram(test_round$agebaby1_round)  
-  str(test_round$agebaby1_round)  
-  summary(test_round$agebaby1_round)  
+  # forgive me epi gods but categorizing age at first birth
+  # tried using cut() but cats did not lign up
+  test <-
+    contraceptors %>% 
+      mutate(agefirstbirth = case_when(
+        is.na(agebaby1_round) ~ "nulliparous",
+        agebaby1_round >= 11 & agebaby1_round <= 14 ~ "11-14",
+        agebaby1_round >= 15 & agebaby1_round <= 19 ~ "15-19",
+        agebaby1_round >= 20 & agebaby1_round <= 24 ~ "20-24",
+        agebaby1_round >= 25 & agebaby1_round <= 29 ~ "25-29",
+        agebaby1_round >= 30 & agebaby1_round <= 34 ~ "30-34",
+        agebaby1_round >= 35 & agebaby1_round <= 39 ~ "35-39",
+        agebaby1_round >= 40 & agebaby1_round <= 44 ~ "40-44",
+        agebaby1_round >= 45 & agebaby1_round <= 49 ~ "45-49"
+      ))
+  
+  # sample_10 <- sample_n(test, 10)
+  # table(sample_10$agebaby1_round, sample_10$agefirstbirth)  
   
