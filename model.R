cb75433740098b7ddc3ce0f37f36b7b3082d8a69
Christine McWilliams <camcwilliams@wisc.edu>
2021-01-02 20:30:22 -0600

    started working on training models

--- model.R
+++ model.R
@@ -1,7 +1,7 @@
 ## Machine Learning Practice Project ##
 ##      Christine McWilliams         ##
 
-# Build and Test XGBoost Model
+# Build and Test ModelS
 
 library(tidyr)
 library(tidyverse)
@@ -9,33 +9,69 @@ library(dplyr)
 library(magrittr)
 library(ggplot2)
 library(xgboost)
+library(caret)
+library(e1071)
+library(randomForest)
 
-# Create test set containing the 2015-2017 data
+# TRAINING AND TESTING ON SUBSETS OF THE FULL DATASET, 2011-2017 DATA
 
-test <-
-  contraceptors %>% 
-    filter(fifteenToSeventeen == 1)
+# Drop vars I'm not going to use for IUD model
+contr_model <- select(contraceptors, -caseid, -constat1, -agebaby1, -elevenToFifteen, -fifteenToSeventeen, -con, -nouse, -agebaby1_round)
 
-table(test$iud)
+# Make outcome a factor to run CART
+contr_model$iud <- as_factor(contr_model$iud)
 
-# Create training set with 60% of 2011-2015 sample
+contr_model <- 
+  contr_model %>% 
+    mutate(iud = case_when(
+      iud == "1" ~ "IUD",
+      iud == "0" ~ "contr not IUD"
+    ))
 
-train <- 
-  contraceptors %>% 
-    filter(is.na(fifteenToSeventeen)) %>% 
-    sample_frac(size = 0.6)
-
-table(train$iud)
-
-# Create cross-validation set with other 40% of 2011-2015 sample
-
-outer_join <- pull(train, caseid)
-
-cv <-
-  contraceptors %>% 
-    filter(!(caseid %in% outer_join))
-
-table(cv$iud)
+table(contr_model$iud)
 
 
-### Next step use caret for processing ###
+str(contr_model)
+
+# Create train/test split
+# Following along from https://www.youtube.com/watch?v=8-LriJaYuQw
+
+inTraining <- createDataPartition(contr_model$iud, p = .75, list = FALSE)
+training <- contr_model[ inTraining,]
+testing <- contr_model[-inTraining,]
+
+
+# 10-fold cross-validation (keeping repeat at 1 for now)
+fitControl <- trainControl(method = "repeatedcv", number = 10, repeats = 1)
+
+
+# ML MODELS
+
+
+# Train CART, checking system time for fun, CART is fast tho
+system.time(
+cartfit1 <- train(iud~., data = training, method = "rpart", trControl=fitControl)
+)
+
+cartfit1
+
+ggplot(cartfit1)
+
+  # Predict on test set
+  pred_iud <- predict(cartfit1, newdata = testing)
+  
+  str(pred_iud)
+  
+  prob_iud <- predict(cartfit1, newdata = testing, type = "prob")
+  
+  head(prob_iud)
+  
+  confusionMatrix(data = pred_iud, testing$iud)
+
+
+# Train Random Forest
+system.time(
+rffit <- train(iud~., data = training, method = "rf", trControl = fitControl)
+)
+
+
