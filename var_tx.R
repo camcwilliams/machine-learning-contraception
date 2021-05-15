e500388dfbe074096f1bc068543c23ad88502193
Christine McWilliams <camcwilliams@wisc.edu>
2021-05-15 15:20:48 -0500

    just some review and tweaks

--- var_tx.R
+++ var_tx.R
@@ -8,7 +8,7 @@ library(dplyr)
 library(magrittr)
 library(ggplot2)
 
-
+setwd("C:/Users/Christine McWilliams/Documents/Box/ML_Practice")
 
 tb <- read_sas("C:/Users/Christine McWilliams/Documents/Box/ML_Practice/nsfg2011_2017.sas7bdat")
 
@@ -20,7 +20,7 @@ table(tb$constat1)
 table(tb$fifteenToSeventeen, useNA = "always")
 
 
-# Create contraception var where those not at risk of unintended pregnancy set to nan
+# Create contraception var where those not at risk of unintended pregnancy set to NA
 tb <-
   tb %>% 
   mutate(con = case_when(
@@ -30,7 +30,7 @@ tb <-
 table(tb$con, useNA = "always")
 
 
-# Not using contraception but at risk of unintended pregnancy
+# Not using contraception but at risk of unintended pregnancy = 1, using contraception = 0
 tb <-
   tb %>% 
     mutate(nouse = case_when(
@@ -49,21 +49,22 @@ contraceptors <-
     ))
 
 str(contraceptors)
+table(contraceptors$iud)
 
 
 # Checking remaining vars to decide what to keep/change
 mean(contraceptors$rscrage)
-histogram(contraceptors$rscrage)
-histogram(contraceptors$agebaby1) # change to one continuous and one categorical var
-table(contraceptors$hieduc) # change to factors
-table(contraceptors$hisprace) # change to factors
-histogram(contraceptors$poverty) # keep as continuous for now (could do flag for discontinuity at 500, similar to diss)
+hist(contraceptors$rscrage) # age can be left as-is
+hist(contraceptors$agebaby1, breaks=20) # years are coded differently
+                                        # categorize to unify
+table(contraceptors$hieduc) # change to factors or recode to relevant schooling (or add discontinuity)
+table(contraceptors$hisprace) # change to factors but consider not including
+hist(contraceptors$poverty) # keep as continuous for now (could do flag for discontinuity at 500, similar to diss)
 table(contraceptors$parity)
 mean(contraceptors$parity) # keep as continuous for now
 table(contraceptors$rwant) # change to factors 
 table(contraceptors$rmarital) # change to factors
 # currins change to factors
-# remove interview timing vars: cmintvw, quarter, phase, intvwyear, fifteentoSeventeen
 # do I need to change 1/0s to factors??
 
 
@@ -72,19 +73,12 @@ fac_vars <- c('hieduc', 'hisprace', 'rwant', 'rmarital', 'curr_ins')
 contraceptors[,fac_vars] <- lapply(contraceptors[,fac_vars], factor)
 str(contraceptors)
 
-
 # Creating discontinuity flag for poverty var, just in case
 contraceptors$poverty_500 <- ifelse(contraceptors$poverty == 500, 1, 0)
 
 
-# Dealing with agebaby1 
-
-  # appears to have different scales for the two different years?
-  summary(nsfg2011_2015$agebaby1)
-  summary(nsfg2015_2017$agebaby1) # sure enough
-
-
-  # fixing that, changing the 11-15 survey's age at first birth to reflect real whole years
+  # fixing agebaby1 discrepancy between years
+  # changing the 11-15 survey's age at first birth to reflect real whole years
   contraceptors <- 
     contraceptors %>% 
       mutate(agebaby1_round = case_when(
@@ -93,7 +87,7 @@ contraceptors$poverty_500 <- ifelse(contraceptors$poverty == 500, 1, 0)
       ))
   
   # forgive me epi gods but categorizing age at first birth
-  # tried using cut() but cats did not lign up
+  # tried using cut() but cats did not line up
   contraceptors <-
     contraceptors %>% 
       mutate(agefirstbirth = case_when(
@@ -111,8 +105,9 @@ contraceptors$poverty_500 <- ifelse(contraceptors$poverty == 500, 1, 0)
   # sample_10 <- sample_n(test, 10)
   # table(sample_10$agebaby1_round, sample_10$agefirstbirth)  
   
-
-# Drop interview timing vars
-contraceptors <- select(contraceptors, -cmintvw, -quarter, -phase, -intvwyear)
+# Keeping interview year info in case it's needed for later reference
+  
+# Dropping interim vars
+contraceptors <- contraceptors %>% select(-agebaby1, -agebaby1_round)
 
 str(contraceptors)
